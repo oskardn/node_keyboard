e1052c84d154452276871f67174c70ac09868b93
Sikelio <contact@sikelio.wtf>
2022-01-11 20:57:57 +0100

    Interface fonctionelle

--- server.js
+++ server.js
@@ -1,9 +1,48 @@
+let express = require('express');
+let path = require('path');
 let sendInput = require('sendinput');
 
-const VOLUME_MUTE = 173, VOLUME_DOWN = 174, VOLUME_UP = 175;
-const MEDIA_NEXT = 176, MEDIA_PREV = 177, MEDIA_STOP = 178, MEDIA_PLAY = 179;
+let app = express();
+let port = 3000;
 
-sendInput.SendInput
-([
-    {val: MEDIA_PLAY, type: 0 }
-]);
+/**
+ * Variables
+ */
+const MEDIA_NEXT = 176, MEDIA_PREV = 177, MEDIA_PLAY_PAUSE = 179; //MEDIA_STOP = 178,;
+
+/**
+ * Redirection vers l'interface web
+ */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+/**
+ * Envoi de données
+ */
+app.post('/prev', (req, res) => {
+    sendInput.SendInput
+    ([
+        {val: MEDIA_PREV, type: 0 }
+    ]);
+    res.redirect('back');
+});
+app.post('/playpause', (req, res) => {
+    sendInput.SendInput
+    ([
+        {val: MEDIA_PLAY_PAUSE, type: 0 }
+    ]);
+    res.redirect('back');
+});
+app.post('/next', (req, res) => {
+    sendInput.SendInput
+    ([
+        {val: MEDIA_NEXT, type: 0 }
+    ]);
+    res.redirect('back');
+});
+
+/**
+ * Écoute du port
+ */
+app.listen(port);
