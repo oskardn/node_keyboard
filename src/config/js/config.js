d279153cb7385704cffd906d3ffa4e5ce35e2a32
Sik <sikeliodev@gmail.com>
2022-05-19 22:44:21 +0200

    remove font-awesome

--- src/config/js/config.js
+++ src/config/js/config.js
@@ -13,21 +13,6 @@ $("button.accueil").on("click", function () {
     window.location.href = "../../home/vue/index.html";
 });
 
-$(".vPasswordVisibility").unbind("click");
-$(".vPasswordVisibility").on("click", () => {
-    if ($("#sToken").attr("type") == "password") {
-        $("#sToken").attr("type", "text");
-
-        $(".vShow").removeClass("cache");
-        $(".vHide").addClass("cache");
-    } else if ($("#sToken").attr("type") == "text") {
-        $("#sToken").attr("type", "password");
-
-        $(".vHide").removeClass("cache");
-        $(".vShow").addClass("cache");
-    }
-});
-
 $("#nPortButton").unbind("click");
 $("#nPortButton").on("click", function () {
     let nPort = $("#nPort").val();
